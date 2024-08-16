7bd84302a16707457515310ef1ec10c5b8412e70
Neil Kuan <46012524+neilkuan@users.noreply.github.com>
2024-08-16 11:49:37 +0800

    chore: use neilkuan/chart-releaser-action@v1.6.2 as release action (#1)
    * chore: use neilkuan/chart-releaser-action@v1.6.2 as release action
    * feat: depends telegraf chart

--- get-helm-depends-update.mjs
+++ get-helm-depends-update.mjs
@@ -1,24 +1,14 @@
 #!/usr/bin/env zx
-const ChartLock = YAML.parse(fs.readFileSync('./Chart.lock', 'utf8'));
-const ChartYaml = YAML.parse(fs.readFileSync('./Chart.yaml', 'utf8'));
+const ChartYaml = YAML.parse(fs.readFileSync('./charts/vm-example-chart/Chart.yaml', 'utf8'));
 let repoList = {
-    ChartLock: [],
     ChartYaml: [],
 };
-ChartLock.dependencies.forEach( repo => {
-  repoList.ChartLock.push({repoName: repo.name, repository: repo.repository});
-});
-
 ChartYaml.dependencies.forEach( repo => {
   repoList.ChartYaml.push({repoName: repo.name, repository: repo.repository});
 });
 
-if (repoList.ChartYaml.length !== repoList.ChartLock.length) {
-    throw new Error(chalk.red(`Please run helm dependency update at local and commit Chart.yaml and Chart.lock`));
-}
-
 const { spawn } = require("child_process");
-for (const [index, lock] of repoList.ChartLock.entries()) {
+for (const [index, lock] of repoList.ChartYaml.entries()) {
     let helmAddRepo;
     if (lock.repository !== repoList.ChartYaml[index].repository){
         helmAddRepo = spawn('helm', ['repo', 'add', '--force-update', repoList.ChartYaml[index].repository.split('@')[1], lock.repository]);
